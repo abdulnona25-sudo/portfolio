ee75226fc0757f22d8b26a794cd61a3d82d38744
Abdelrahman <abdulnona25@gmail.com>
2026-04-29 16:35:40 +0100

    updatee

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,7 +32,7 @@ export default function Projects() {
         ar: "موقع حجز حلاق بواجهة حديثة وأنيقة، يشمل قائمة الخدمات وتدفق الحجز وتصميماً متجاوباً.",
       },
       image: barber,
-      live: "barber-flame-phi.vercel.app",
+      live: "https://barber-flame-phi.vercel.app",
       code: "https://github.com/abdulnona25-sudo/barber-website",
       blank: false,
     },
